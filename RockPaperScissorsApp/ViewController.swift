8057ca50bd75ae1ec05b7d1a8a0862fb5d9d8aef
Isa Fontana <isfon8@gmail.com>
2022-04-04 14:09:33 -0600

    progress

--- RockPaperScissorsApp/ViewController.swift
+++ RockPaperScissorsApp/ViewController.swift
@@ -8,7 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+//IMAGE VIEWS:
 
+    @IBOutlet weak var computerOutputImageView: UIImageView!
+
+    @IBOutlet weak var myOutputImageView: UIImageView!
+    
+    @IBOutlet weak var rockImageView: UIImageView!
+    
+    @IBOutlet weak var paperImageView: UIImageView!
+    
+    @IBOutlet weak var scissorsImageView: UIImageView!
+    
+//SCORES:
+    @IBOutlet weak var computerScoreLabel: UILabel!
+    
+    var computerScore: Int = 0
+    
+    
+    @IBOutlet weak var myScoreLabel: UILabel!
+    
+    var myScore: Int = 0
+    
+//BUTTONS:
+    @IBAction func onRockButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func onPaperButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func onScissorsButtonTapped(_ sender: UIButton) {
+    }
+    
+    var icons: [UIImage] = [UIImage(named: "rock")!, UIImage(named: "paper")!, UIImage(named:  "scissors")!
+    ]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
